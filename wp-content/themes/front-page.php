92d94169c4ce7462eb5c4f79e355fd21d5f9375a
root <root@ip-172-31-8-219.ap-southeast-2.compute.internal>
2024-04-03 04:22:32 +0000

    something like that

---
+++ wp-content/themes/front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="w-full h-screen flex flex-col justify-center bg-red-500 py-4">
+    <span>testDee</span>
+    <div data-aos="fade-up" data-aos-duration="1000">
+         <span class="text-blue-500">tetet</span>
+    </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/wordpress-with-tailwind/wordpress-with-tailwind/front-page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<div class="w-full h-screen flex flex-col justify-center items-center bg-slate-200 py-4">
+    <span>testDee</span>
+    <div data-aos="fade-up" class=" rounded bg-amber-200 p-4">
+         <span class="text-blue-500">tetet</span>
+    </div>
+</div>
+<div class="w-full h-screen flex flex-col justify-center items-center bg-slate-200 py-4">
+    <span>testDee</span>
+    <div data-aos="fade-up" class=" rounded bg-amber-200 p-4">
+         <span class="text-blue-500">tetet</span>
+    </div>
+</div>
+<div class="w-full h-screen flex flex-col justify-center items-center bg-slate-200 py-4">
+    <span>testDee</span>
+    <div data-aos="fade-up" class="p-0 rounded bg-amber-200">
+         <span class="text-blue-500">tetet</span>
+    </div>
+</div>
+
+<?php get_footer(); ?>
